598c3a23635bf5a66c9a9835b89b4508ed1a7310
Ryota <disneyresort1983@icloud.com>
2023-09-07 22:59:32 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import MemoPage from './memo/MemoPage'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-primary text-white display-4'>React</h1>
+      <div className='container'>
+        <h4 className='my-3'>Memo.</h4>
+        <MemoPage />
+      </div>
+    </div>
+  )
+}
+
+export default App
